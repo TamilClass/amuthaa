7965a1f088376b9afee763cbbe16dbfcea3aaf76
Ashwin Balamohan <ashwin@logeo.io>
2012-12-24 01:40:28 -0500

    Fixed errors in model definitions

--- models.py
+++ models.py
@@ -6,10 +6,9 @@ class PartOfSpeech(models.Model):
     tamil           = models.CharField(max_length=20)   # Tamil Part of Speech
     english         = models.CharField(max_length=20)   # Equivalent English PoS 
     
-    # ppedagogical information
+    # pedagogical information
     example         = models.TextField(null=True, blank=True)
     description     = models.TextField(null=True, blank=True)
-
     
     # string representation of word: display Tamil
     def __unicode__(self):
@@ -31,17 +30,12 @@ class Word(models.Model):
     # lesson number in which word was taught
     lesson          = models.IntegerField(null=True, blank=True)
     
-    # whether word was directly taught 
-    # the alternative is if the word is implied, i.e. it isn't directly taught
-    #   but can be figured out with the knowledge the student has at that lesson
-    taught          = models.BooleanField(null=True, blank=True, default=False)
-    
     # whether a word has been verified by an administrator
     # this will allow words that have been crowdsourced to be checked for 
     # spelling, etc. before being used in lessons and exercises
-    verified        = models.BooleanField(null=True, blank=True, default=False)
+    verified        = models.BooleanField(blank=True, default=False)
 
-    audio_file      = models.FileField(upload_to='')
+    audio_file      = models.FileField(upload_to='/media/words/')
 
     
     # string representation of word: display Tamil
@@ -63,14 +57,8 @@ class Letter(models.Model):
     # whether the letter is a vowel, consonant or combination
     type        = models.CharField(max_length=2, choices=LETTER_TYPES)
     
-    # lesson number in which word was taught
-    lesson      = models.IntegerField()
-    
-    # The "Tamil Course for European Schools" lesson number
-    tces_lesson = models.IntegerField()
-    
     # audio file (preferably .ogg format) with pronunciation     
-    audio_file  = models.FileField()
+    audio_file  = models.FileField(upload_to='/media/letters/')
     
     # string representation of the letter: display Tamil
     def __unicode__(self):
