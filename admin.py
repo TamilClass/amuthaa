ea39bde0fc01faeae20df5472619506f007438be
Ashwin Balamohan <ashwin@logeo.io>
2012-12-24 21:38:52 -0500

    Modified Parts of Speech to only show English and Tamil values

--- admin.py
+++ admin.py
@@ -1,7 +1,11 @@
 from apps.amuthaa.models import PartOfSpeech, Word, Letter, Rule
 from django.contrib import admin
 
-admin.site.register(PartOfSpeech)
+class PartOfSpeechAdmin(admin.ModelAdmin):
+    list_display = ('english', 'tamil')
+
+admin.site.register(PartOfSpeech, PartOfSpeechAdmin)
 admin.site.register(Letter)
 admin.site.register(Word)
 admin.site.register(Rule)
+
