521c4534f506341c2e1cfc5163dbf69b8a4b182b
abmohan <ashwin@logeo.io>
2013-01-04 00:49:56 -0500

    Added skeletal functions within Chol

--- lib/Chol.py
+++ lib/Chol.py
@@ -2,39 +2,90 @@
 #!/usr/bin/python
 
 # import Ezhuthu class
-try:
-    from learntamil.apps.amuthaa.lib.Ezhuthu import Ezhuthu
-except ImportError:
-    import sys
-    from os.path import join, abspath, dirname
-    parentpath = abspath(join(dirname(__file__), '..'))
-    srcpath = join(parentpath, 'lib')
-    sys.path.append(srcpath)
-    from Ezhuthu import Ezhuthu
+from Ezhuthu import Ezhuthu
 
+
+# Add skeletal outline for functions. Removed redundant function. Modified import command
 
 class Chol:
     """
     Module to handle the processing of Tamil words
     """
+
+    @staticmethod
+    def validate_word(word=u''):
+        """ docstring """   
+        #TODO: add docstring
+        
+        #TODO: implement method 
+        
+    @staticmethod
+    def validate_noun_class(noun_class = 0):
+        """ docstring """
+        #TODO: add docstring
+        
+        #TODO: implement method
+
     
     @staticmethod
-    def get_letters(word = ""):
-        #TODO: Implement this
-        pass
-    
-    
-    
-    @staticmethod    
-    def final_letter(word = ""):
-        """ 
-        Returns the final letter in a word
-        """
+    def validate_verb_class(noun_class = 0):
+        """ docstring """
+        #TODO: add docstring
         
-        # ensure that word has at least one character
-        if len(word) == 0:
-            raise TypeError("Input must be at least character long")
+        #TODO: implement method
+    
+    
+    @staticmethod
+    def get_letters(word=u''):
+        """ docstring """   
+        #TODO: add docstring
         
-        # return last letter of string
-        #TODO: Fix this so that it takes graphemes into account
-        return word[-1]    
+        #TODO: Implement method
+        
+    @staticmethod
+    def num_syllables(word=u''):
+        """ docstring """
+        #TODO: add docstring
+        
+        #TODO: Implement method
+        
+    
+    @staticmethod
+    def add_noun_suffix(word = u'', word_class=0, suffix = ''):
+        """ docstring """
+        #TODO: add docstring
+        
+        #TODO: Implement method
+    
+    
+    @staticmethod
+    def pluralize(word = u''):
+        """ docstring """
+        #TODO: add docstring
+        
+        #TODO: Implement method
+        
+    
+    @staticmethod
+    def get_conjugation(verb=u'', verbclass=0, tense='PRESENT'):
+            """ docstring """
+        #TODO: add docstring
+        
+        #TODO: Implement method
+        
+    
+    @staticmethod
+    def get_noun_root(word):
+            """ docstring """
+        #TODO: add docstring
+        
+        #TODO: Implement method
+        
+    
+    @staticmethod
+    def get_noun_root(word):
+            """ docstring """
+        #TODO: add docstring
+        
+        #TODO: Implement method
+    
