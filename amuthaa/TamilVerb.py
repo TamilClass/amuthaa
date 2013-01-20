3c592af75c69843f1daf9d9da7ffbd3d231dae28
Ashwin Balamohan <ashwin@logeo.io>
2013-01-20 12:09:36 -0500

    Added PRONOUN_SUFFIX dictionary to map pronounss to their respective suffixes. Also made some PEP8-compliant fixes to white spacing.

--- amuthaa/TamilVerb.py
+++ amuthaa/TamilVerb.py
@@ -6,17 +6,31 @@ from TamilWord import TamilWord
 
 
 class TamilVerb(TamilWord):
-    
+
+    # Dictionary to map a pronoun with its respective suffix
+    PROUNOUN_SUFFIXES = {
+                         u'நான்': u'ஏன்',
+                         u'நீ': u'ஆய்',
+                         u'நீர்': u'ஈர்',
+                         u'நீங்கள்': u'ஈர்கள்',
+                         u'அவன்': u'ஆன்',
+                         u'அவள்': u'ஆள்',
+                         u'அவர்': u'ஆர்',
+                         u'நாங்கள்': u'ஓம்',
+                         u'நாம்': u'ஓம்',
+                         u'அவர்கள்': u'ஆர்கள்',
+                         u'அது': u'அது',
+                         u'அவை': u'அன',
+                         }
+
     @staticmethod
-    def validate_class(noun_class = 0):
+    def validate_class(noun_class=0):
         """ docstring """
-        
+
         #TODO: implement method
-    
-    
 
     @staticmethod
     def conjugate(verb=u'', verbclass=0, tense='PRESENT'):
             """ docstring """
-        
+
         #TODO: Implement method
