775ab7a43ddb353e97d28ea457799a33c5a07a72
abmohan <ashwin@logeo.io>
2013-01-21 16:17:18 -0500

    PEP8 formatting and added constructor

--- amuthaa/TamilVerb.py
+++ amuthaa/TamilVerb.py
@@ -6,17 +6,21 @@ from TamilWord import TamilWord
 
 
 class TamilVerb(TamilWord):
-    
+
+    def __init__(self, root=u'', infinitive=u'', conjunctive=u''):
+        _classnum = None
+        _root = root
+        _infinitive = infinitive
+        _conjunctive = conjunctive
+
     @staticmethod
-    def validate_class(noun_class = 0):
+    def validate_class(noun_class=0):
         """ docstring """
-        
-        #TODO: implement method
-    
-    
+
+        # TODO: implement method
 
     @staticmethod
     def conjugate(verb=u'', verbclass=0, tense='PRESENT'):
             """ docstring """
-        
-        #TODO: Implement method
+
+        # TODO: Implement method
